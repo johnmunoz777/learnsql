936b40c94068f127944bd786967f04dfcc7c32d4
shanti <johnmunoz777@outlook.com>
2024-12-30 14:17:52 -0700

    added exchange rate for peru

--- api_example.py
+++ api_example.py
@@ -19,3 +19,20 @@ for currency_code in currencies:
     response = requests.get(url)
     response = response.json()
     print(response['rates']['USD'])
+    
+
+#'PEN': 3.73,
+base_url = "https://api.exchangerate-api.com/v4/latest/"
+currencies = ['EGP', 'GMD', 'CLP','PEN']
+
+for currency_code in currencies:
+    url = f'{base_url}{currency_code}'
+    response = requests.get(url)
+    response = response.json()
+    print(f'The exchange rate from {currency_code} to USD is {response["rates"]["USD"]}.')
+    if currency_code== 'PEN':
+      print("TIME TO MAKE THAT MONEY BABY")
+      url = f'{base_url}{"USD"}'
+      response = requests.get(url)
+      response = response.json()
+      print(f'The exchange rate from USD to pen is {response["rates"]["PEN"]}.')
